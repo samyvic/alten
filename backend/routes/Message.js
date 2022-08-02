5500300401e7cd8daeaaf3fb5b89ec3c7da18db2
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-08-02 02:17:28 +0200

    add routes

---
+++ backend/routes/Message.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+
+
+const router = express.Router();
+
+
+
+router.route('/')
+        .get()
+        .post()
+
+        
+router.route('/:id')
+        .get()
+        .patch()
+        .delete()
