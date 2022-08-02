566fda76ea11a79c504e2933a70605f35b9d2ca3
ahmadyasser01 <ahmadyasser2814@gmail.com>
2022-08-02 02:12:57 +0200

    added message model

---
+++ backend/models/messages.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+
+const messageSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,"User name is required"],
+    },
+    email:{
+        type:String,
+        required: [true,"Email is required"],
+        lowercase: true,
+        trim: true,
+        validate:[validator.isEmail,"Email is required"]
+    },
+    phoneNumber:{
+        type:String,
+        required: [true,"Phone number is required"]
+    },
+    title:{
+        type:String,
+        required: [true,"Title is required"]
+    },
+    description:{
+        type:'string',
+        required: [true,"Description is required"]
+    },
+    date:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const Message = mongoose.model('Message',messageSchema);
+
+export default Message;
